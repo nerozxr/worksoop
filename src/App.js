4017181b8d26dcea93c4d7c77e002733f7d74767
nerozxr <89910987+nerozxr@users.noreply.github.com>
2022-01-16 14:39:42 +0700

    first submit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import "./Assets/App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="navbar">header</div>
+      <div className="Section">
+        <div className="card Section-body-1">1</div>
+        <div className="card Section-body-2">2</div>
+        <div className="card Section-body-3">3</div>
+        <div className="card Section-body-4">4</div>
+        <div className="card Section-body-5">5</div>
+        <div className="card Section-body-6">6</div>
+      </div>
+
+      <div className="Body">
+        <div className="Body-Anak Body-1">1</div>
+        <div className="Body-Anak Body-2">2</div>
+        <div>1</div>
+        <div>2</div>
+        <div className="Body-Anak Body-3">3</div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
